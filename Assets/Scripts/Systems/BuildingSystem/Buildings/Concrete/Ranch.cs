f4b50928149a3750fadcd31a0f2bcd3403cef6af
our0boros <our0boros@163.com>
2025-06-06 00:11:40 +1000

    [UPDATE] 新增Buff建筑在新建与拆除时的Buff登记方法

--- Assets/Scripts/Systems/BuildingSystem/Buildings/Concrete/Ranch.cs
+++ Assets/Scripts/Systems/BuildingSystem/Buildings/Concrete/Ranch.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Ranch : ProductionBuilding
 {
-    [Header("牧场专属")]
-    public LivestockSubType animalType = LivestockSubType.Cattle;
-    
-    public new void OnTryBuilt()
-    {
-        status = BuildingStatus.Active;
-        SetupProductionRule();
-        Debug.Log($"牧场建造完成，位置: {string.Join(" ", positions)}");
-    }
-    
     public override void OnUpgraded()
     {
         Debug.Log($"牧场升级到等级 {currentLevel}");
