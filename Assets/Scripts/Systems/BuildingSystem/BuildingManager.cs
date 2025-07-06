8abdfae7be3a2d8887145f97ed3dab9101826565
Star Chen <cxsgdsg1@gmail.com>
2025-07-06 12:27:38 +0800

    [PLUGINS] 移除了NaughtyAttribute插件以及解决因移除带来的报错。

--- Assets/Scripts/Systems/BuildingSystem/BuildingManager.cs
+++ Assets/Scripts/Systems/BuildingSystem/BuildingManager.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 using UnityEngine.Rendering;
 
 [DisallowMultipleComponent]
