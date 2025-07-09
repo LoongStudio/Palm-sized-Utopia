1c2d4c2fc0b4f81d2b0d14a457b2fe8a4f625ee7
Star Chen <cxsgdsg1@gmail.com>
2025-07-09 11:16:29 +0800

    [UPDATE] 调整商店物品数据结构，在资源不足时显示为另一样式并且不可互动

--- Assets/Scripts/FrontEnd/UI/Items/ShopItemData.cs
+++ Assets/Scripts/FrontEnd/UI/Items/ShopItemData.cs
@@ -5,13 +5,15 @@ public class ShopItemData{
     public string description;
     public int price;
     public ResourceType priceType;
+    public int priceSubType;
     public Sprite icon;
 
-    public ShopItemData(string name, string description, int price, ResourceType priceType, Sprite icon){
+    public ShopItemData(string name, string description, int price, ResourceType priceType, int priceSubType, Sprite icon){
         this.name = name;
         this.description = description;
         this.price = price;
         this.priceType = priceType;
+        this.priceSubType = priceSubType;
         this.icon = icon;
     }
 }
